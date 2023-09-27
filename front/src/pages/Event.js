7213feae349e9cb14962a5633b73db99cfa03821
Serj52 <blackday52@mail.ru>
2023-09-27 17:57:11 +0300

    added dateEvent

--- front/src/pages/Event.js
+++ front/src/pages/Event.js
@@ -2,11 +2,14 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import "./style.css"; 
+import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs';
+
+import "./style.css";
 
 function Event() {
     const variantResponse = [
@@ -20,12 +23,24 @@ function Event() {
         }
     ]
 
-    const [value, setValue] = React.useState('Нет');
+    const [limit, setLimit] = React.useState('Нет');
 
-    const handleChange = (e) => {
+
+    const handleChangeLimit = (e) => {
         console.log(e.target.value)
-        setValue(e.target.value)
+        setLimit(e.target.value)
     }
+
+    const [dateEvent, setDateEvent] = React.useState(dayjs());
+    const [dateRecord, setDateRecord] = React.useState(dayjs());
+    const [dateEventExist, setDateEventExist] = React.useState("Нет");
+
+    const handleChangeDate = (e) => {
+        console.log(e.target.value)
+        setDateEventExist(e.target.value)
+    }
+    
+    
 
 
     return (
@@ -35,47 +50,74 @@ function Event() {
                 '& .MuiTextField-root': { m: 1, width: '25ch' },
             }}
             noValidate
-            autoComplete="off"
-        >
+            autoComplete="off">
             <div className='textField'>
-            <TextField
-                id="outlined-search"
-                label="Название мероприятия"
-                type="search"
-                style={{margin:25}}
-            />
+                <TextField
+                    id="outlined-search"
+                    label="Название мероприятия"
+                    type="search"
+                    style={{ margin: 25 }}
+                />
 
-            <TextField
-                id="outlined-select-currency"
-                select
-                label="Выберите"
-                defaultValue={value}
-                helperText="Ограничение числа участников"
-                onChange={handleChange}
-                style={{margin:25}}
+                <TextField
+                    id="outlined-select-currency"
+                    select
+                    label="Выберите"
+                    defaultValue={limit}
+                    helperText="Ограничение числа участников"
+                    onChange={handleChangeLimit}
+                    style={{ margin: 25 }}
+                >
+                    {variantResponse.map((option) => (
+                        <MenuItem key={option.value} value={option.value}>
+                            {option.label}
+                        </MenuItem>
+                    ))}
+                </TextField>
 
-            >
-                {variantResponse.map((option) => (
-                    <MenuItem key={option.value} value={option.value}>
-                        {option.label}
-                    </MenuItem>
-                ))}
+                <TextField
+                    id="outlined-number"
+                    label="Число участников"
+                    type="number"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    style={{ margin: 25, display: limit != 'Нет' ? "block" : "none" }}
+                />
+                <TextField
+                    id="outlined-select-currency"
+                    select
+                    label="Выберите"
+                    defaultValue={dateEventExist}
+                    helperText="Дата мероприятия"
+                    onChange={handleChangeDate}
+                    style={{ margin: 25 }}
+                >
+                    {variantResponse.map((option) => (
+                        <MenuItem key={option.value} value={option.value}>
+                            {option.label}
+                        </MenuItem>
+                    ))}
+                </TextField>
 
-            </TextField>
+                <LocalizationProvider dateAdapter={AdapterDayjs} style={{ margin: 25, display: dateEventExist != 'Нет' ? "block" : "none" }}>
+                    <DemoContainer components={['DateCalendar', 'DateCalendar']}>
+                        <DemoItem label="Выберите дату мероприятия">
+                        <DateCalendar value={dateEvent} onChange={(newValue) => setDateEvent(newValue)} />
+                        </DemoItem>
+                    </DemoContainer>
+                </LocalizationProvider >
+                
 
-            <TextField
-                id="outlined-number"
-                label="Число участников"
-                type="number"
-                InputLabelProps={{
-                    shrink: true,
-                }}
-                style={{ margin:25, display: value != 'Нет' ? "block" : "none" }}
-            />
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DemoContainer components={['DateCalendar', 'DateCalendar']}>
+                        <DemoItem label="Выберите срок записи">
+                        <DateCalendar value={dateRecord} onChange={(newValue) => setDateRecord(newValue)} />
+                        </DemoItem>
+                    </DemoContainer>
+                </LocalizationProvider>
 
-
-
-        </div>
+            </div>
         </Box >
 
 
